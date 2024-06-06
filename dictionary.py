01c96d47a38e8bcd0482f1b78d6ca1754a38a940
Herbert97 <falcon9718@hotmail.com>
2024-06-05 18:35:55 -0600

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+#dictionary
+dictionary= {
+    'a':[1,2,3],
+    'b':'hello',
+    'x': True
+}
+my_list = [{
+    'a':[1,2,3],
+    'b':'hello',
+    'x': True
+
+},
+{
+    'a':[4,5,6],
+    'b':'hello',
+    'y': True  
+}
+]
+print(my_list[0]['a'][2])
+print(dictionary['a'][1])
