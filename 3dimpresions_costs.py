5d55fbc92e7526ac366c3f4e10bac3d77f9b70ee
Herbert97 <falcon9718@hotmail.com>
2024-12-08 01:32:56 -0600

    calcular el precio de la impresion 3d

--- 3dimpresions_costs.py
+++ 3dimpresions_costs.py
@@ -2,12 +2,32 @@
 
 
  # entrada
-print('Calculando el costo de la impresion por gramos: ')
-print('Dame el costo del kilogramo de filamento y la cantidad en gramos de filamento de la pieza: ')
-base,altura = float(input()), float(input()) #se leen dos datos a la vez separados por enter
-print('dame el tiempo de impresion: ')
+#variables del costo
+print('Calculando el costo de la impresion')
+
+#se leen los datos separados por enter
+print('Dame el peso de la piezas en gr: ')
+peso = float(input())
+
+print('dame el costo del filamento')
+costo_del_filamento = float(input())
+
+print('dame el tiempo de impresion en horas: ')
 tiempo_impresion=float(input())
+
+print('dame el costo de la mano de obra: ')
+mano_de_obra= float(input())
+#variables fijas estos valores pueden cambiar dependiendo del trabajo.
+costo_kw_hrs = 0.81
+watts_maquina = 0.35
+ganancia= 0.4
+mantenimiento=0.5
 #Proceso
-area= base*altura /2
+#costo del filamento por gr.
+costo_por_gramos=(peso/1000)*costo_del_filamento
+costo_energia= (tiempo_impresion*costo_kw_hrs*watts_maquina)
+subtotal=(costo_por_gramos+costo_energia+mantenimiento)
+total=(subtotal*(1+ganancia))
+
 #Salida
-print(f'Para un triangulo de base {base} y altura {altura} el area es {area}')
+print(f'Para una impresion 3d con un tiempo de: {tiempo_impresion} tiene un costo por gramos de: {costo_por_gramos}  el subtotal es de: {subtotal} dando un total por pieza de {total}')
